e551d6d0c684400b8ffd7ae91f04b576dd60573a
akoitanov <abzal.koitanov@gmail.com>
2019-07-24 15:00:31 -0700

    almost done

---
+++ src/App.js
@@ -0,0 +1,87 @@
+//imports dependencies and files
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import FriendCard from "./components/FriendCard";
+import Footer from "./components/Footer";
+import epl from "./epl.json";
+import "./App.css";
+
+//sets state to 0 or empty
+class App extends Component {
+  state = {
+    team,
+    clickedTeam: [],
+    score: 0
+  };
+
+//when you click on a card ... the team is taken out of the array
+  imageClick = event => {
+    const currentTeam = event.target.alt;
+    const TeamAlreadyClicked =
+      this.state.clickedTeam.indexOf(currentTeam) > -1;
+
+//if you click on a team that has already been selected, the game is reset and cards reordered
+    if (TeamAlreadyClicked) {
+      this.setState({
+        team: this.state.team.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedTeam: [],
+        score: 0
+      });
+        alert("You lose. Play again?");
+
+//if you click on an available team, your score is increased and cards reordered
+    } else {
+      this.setState(
+        {
+          team: this.state.team.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedTeam: this.state.clickedTeam.concat(
+            currentTeam
+          ),
+          score: this.state.score + 1
+        },
+//if you get all 10 team corrent you get a congrats message and the game resets        
+        () => {
+          if (this.state.score === 12) {
+            alert("Yay! You Win!");
+            this.setState({
+              team: this.state.team.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedTeam: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+//the order of components to be rendered: navbar, jumbotron, friendcard, footer 
+  render() {
+    return (
+      <div>
+        <Navbar 
+          score={this.state.score}
+        />
+        <Jumbotron />
+        <div className="wrapper">
+          {this.state.team.map(team => (
+            <FriendCard
+              imageClick={this.imageClick}
+              id={team.id}
+              key={team.id}
+              image={team.image}
+            />
+          ))}
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+export default App;
